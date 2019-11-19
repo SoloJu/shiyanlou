f95de81b1df5ddb60972a8512c7271689bdedd14
soloju <soloju@gmail.com>
2019-11-19 18:38:34 +0800

    change all

--- calculator.py
+++ calculator.py
@@ -3,38 +3,35 @@
 
 import sys
 
-my_income = sys.argv[1]
+if __name__ = '__main__'
+    if len(sys.argv) != 2:
+        print('Parameter Error')
+        sys.exit()
 
-# check the entery is a normal salary.
+    try:
+        income = int(sys.argv[1])
+    except ValueError:
+        print('Parameter Error')
+        sys.exit()
 
-def check_input(arg1):
-    if isinstance(int(arg1), int):
-        return int(arg1)
+    value = income - 5000
+
+    if value <= 0:
+        result = 0
+    elif value <= 3000:
+        result = value * 0.03 - 0
+    elif value <= 12000:
+        result = value * 0.1 - 210
+    elif value <= 25000:
+        reslult = value * 0.2 - 1410
+    elif value <= 35000:
+        result = value * 0.25 -2660
+    elif value <= 55000:
+        result = value * 0.30 - 4410
+    elif valut <= 80000:
+        result = value * 0.35 - 7160))
     else:
-        raise Exception('The input is not an int!')
+        result = value * 0.45 - 15160))
 
-def calu_tax(salary):
-    excess = salary - 5000
-    if excess <= 0:
-        print('{:.2f}'.format(0))
-    elif excess > 0 and excess <= 3000:
-        print('{:.2f}'.format(excess * 0.03 - 0))
-    elif excess > 3000 and excess <= 12000:
-        print('{:.2f}'.format(excess * 0.1 - 210))
-    elif excess > 12000 and excess <= 25000:
-        print('{:.2f}'.format(excess * 0.2 - 1410))
-    elif excess > 25000 and excess <= 35000:
-        print('{:.2f}'.format(excess * 0.25 -2660))
-    elif excess > 35000 and excess <= 55000:
-        print('{:.2f}'.format(excess * 0.30 - 4410))
-    elif excess > 55000 and excess <= 80000:
-        print('{:.2f}'.format(excess * 0.35 - 7160))
-    else:
-        print('{:.2f}'.format(excess * 0.45 - 15160))
-
-try:
-    my_income_num = check_input(my_income)
-    print(calu_tax(my_income_num))
-except:
-    print("Unexpected error:", sys.exc_info()[0])
+    print(format(result, '.2f'))
 
