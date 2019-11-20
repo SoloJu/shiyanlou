52d76f4a962a5eae65752cedcff6019eb7ce1478
soloju <soloju@gmail.com>
2019-11-20 22:49:23 +0800

    last one

--- calculator.py
+++ calculator.py
@@ -41,11 +41,6 @@ if __name__ == '__main__':
         person_tax = taxed(person_income)
         person_dict[person_nu] = person_income - person_tax
 
-    print(person_dict)
-
-
-
-
-
-    print(format(result, '.2f'))
+    for key, value in person_dict:
+        print('{}:{.2f}'.format(key, value))
 
