e48d9889324eabf60a3eabc0d4dcf62b1f0201c5
soloju <soloju@gmail.com>
2019-11-21 00:26:26 +0800

    last one

--- calculator.py
+++ calculator.py
@@ -2,45 +2,56 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from collections import namedtuple
 
-def taxed(income):
-    value = income - 5000 - income * 0.165
+IncomeTaxQuickLookupItem = namedtuple(
+    'IncomeTaxQuickLookupItem',
+    ['start_point', 'tax_rate', 'quick_subtractor']
+)
 
-    if value <= 0:
-        result = 0
-    elif value <= 3000:
-        result = value * 0.03 - 0
-    elif value <= 12000:
-        result = value * 0.1 - 210
-    elif value <= 25000:
-        reslult = value * 0.2 - 1410
-    elif value <= 35000:
-        result = value * 0.25 -2660
-    elif value <= 55000:
-        result = value * 0.30 - 4410
-    elif value <= 80000:
-        result = value * 0.35 - 7160
-    else:
-        result = value * 0.45 - 15160
-    return result
+INCOME_TAX_START_POINT = 5000
+
+INCOME_TAX_QUICK_LOOKUP_TABLE = [
+    IncomeTaxQuickLookupItem(80000, 0.45, 15160),
+    IncomeTaxQuickLookupItem(55000, 0.35, 7160),
+    IncomeTaxQuickLookupItem(35000, 0.30, 4410),
+    IncomeTaxQuickLookupItem(25000, 0.25, 2660),
+    IncomeTaxQuickLookupItem(12000, 0.2, 1410),
+    IncomeTaxQuickLookupItem(3000, 0.1, 210),
+    IncomeTaxQuickLookupItem(0.00, 0.03, 0)
+]
+
+SOCIAL_INSURANCE_MONEY_RATE = {
+    'endowment_insurance': 0.08,
+    'medical_insurance': 0.02,
+    'unemployment_insurance': 0.005,
+    'employment_injury_insurance': 0,
+    'maternity_insurance': 0,
+    'public_accumulation_funds': 0.06
+}
+
+def calc_income_tax_and_remain(income):
+    social_insurance_money = income * sum(SOCIAL_INSURANCE_MONEY_RATE)
+    real_income = income - social_insurance_money
+    taxable_part = real_income - INCOME_TAX_START_POINT
+    for item in INCOME_TAX_QUICK_LOOKUP_TABLE:
+        if taxable_part > item.start_point:
+            tax = taxable_part * item.tax_rate - item.quick_subtractor
+            return '{:.2f}'.format(tax), '{:.2f}'.format(real_income - tax)
+    return '0.00', '{:.2f}'.format(real_income)
+
+def main():
+    for item in sys.argv[1:]:
+        employee_id, income_string = item.split(':')
+        try:
+            income = int(income_string)
+        except ValueError:
+            print('Paramenter Error')
+            continue
+
+    _, remain = calc_income_tax_and_remain(income)
+    print('{}:{}'.format(employee_id, remain))
+
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Parameter Error')
-        sys.exit()
-
-    person_dict = {}
-    for items in sys.argv[1:]:
-        each_person = items.split(':')
-        person_nu = each_person[0]
-        try:
-            person_income = int(each_person[1])
-        except ValueError:
-            print('Parameter Error')
-            sys.exit()
-        person_tax = taxed(person_income)
-        person_dict[person_nu] = person_income - person_tax - person_income * 0.165
-
-    for key, value in person_dict.items():
-        print('{0}:{1:.2f}'.format(key, value))
-
+    main()
