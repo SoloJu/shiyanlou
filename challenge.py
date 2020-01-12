f21a9d795959e957b50fb7a9bdcd0811f37d1dc9
soloju <soloju@gmail.com>
2020-01-12 18:41:34 +0800

    all done

--- challenge.py
+++ challenge.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from openpyxl import load_workbook
+from openpyxl import Workbook
 import datetime
+import os
 
 
 def combine():
@@ -21,5 +23,36 @@ def combine():
     wb.save('courses.xlsx')
 
 
+def split():
+    year_list = []
+    sheets_title = []
+    file_list = []
+    row_list = []
+    wb_old = load_workbook('courses.xlsx')
+    ws_combine = wb_old['combine']
+    for x in range(2, len(ws_combine['A'])):
+        if ws_combine['A'][x].value.year in year_list:
+            continue
+        else:
+            year_list.append(ws_combine['A'][x].value.year)
+    for x in range(len(year_list)):
+        sheets_title.append(str(year_list[x]))
+        file_list.append(str(year_list[x]) + '.xlsx')
+    for x in range(2, len(ws_combine['A'])):
+        i = year_list.index(ws_combine['A'][x].value.year)
+        if os.path.isfile(os.path.dirname('courses.xls') + file_list[i]):
+            wb = load_workbook(file_list[i])
+            ws = wb[sheets_title[i]]
+        else:
+            wb = Workbook()
+            ws = wb.active
+            ws.title = sheets_title[i]
+        row_list = [ws_combine['A'][x].value, ws_combine['B'][x].value,
+                    ws_combine['C'][x].value, ws_combine['D'][x].value]
+        ws.append(row_list)
+        wb.save(file_list[i])
+
+
 if __name__ == '__main__':
     combine()
+    split()
